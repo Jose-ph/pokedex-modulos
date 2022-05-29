5af9f28994d616d88263b25d5f07bbe0cdea9783
Jose <jose_esteban_basualdo@hotmail.com>
2022-05-29 11:44:11 -0300

    adds async/await into services

--- services/services.js
+++ services/services.js
@@ -1,6 +1,6 @@
 //const BASE_URL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1126";
 
-export function getPokemonsFromApi(offset = 0) {
+/* export function getPokemonsFromApi(offset = 0) {
   return fetch(
     `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`
   ).then((response) => response.json());
@@ -10,4 +10,21 @@ export function getPokemonById(id) {
   return fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`).then((response) =>
     response.json()
   );
+}
+ */
+
+/*CAMBIO A ASYNC AWAIT  */
+
+export async function getPokemonsFromApi(offset = 0) {
+  let pokemons = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`
+  );
+
+  return pokemons.json();
+}
+
+export async function getPokemonById(id) {
+  let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+
+  return pokemon.json();
 }
