d7d92fcd8079012b89d4039b610b82d3e5b4b965
Jose <jose_esteban_basualdo@hotmail.com>
2022-06-22 10:37:27 -0300

    adds cy.intercept
    to mock the api response and get the data from a fixture

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,45 +1,57 @@
+/// <reference types="Cypress" />
+
 const URL = "http://127.0.0.1:8080/";
 const hashURL = "http://127.0.0.1:8080/#";
 const numberOfPages = 57;
 const cardsPerPage = 20;
 
-context("Pokedex", () => {
-  before(() => {
-    cy.visit(URL);
-  });
-
-  it("checks number of pages in pagination", () => {
-    cy.get("#pagination")
-      .find(".page-link")
-      .should("have.length", numberOfPages);
-  });
-
-  it("checks number of pokemon cards", () => {
-    cy.get("#pokemon-cards").find(".card").should("have.length", cardsPerPage);
-  });
-
-  it("changes page", () => {
-    cy.get("#pagination")
-      .find(".page-link")
-      .then((pages) => {
-        let $pages = [];
-
-        pages.each(function (i, page) {
-          $pages.push(page);
-        });
-        console.log($pages);
-        let defaultCards = [];
-        cy.get(".card").then((cards) => {
-          cards.each(function (i, card) {
-            defaultCards.push(card);
-          });
-          console.log(defaultCards);
-
-          $pages[1].click();
-
-          //I expect to get the newCards
-          cy.get(".card");
-        });
-      });
-  });
+before(() => {
+  cy.visit(URL);
 });
+
+it("Mocks the API response", () => {
+  cy.intercept("GET", "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20", {
+    fixture: "firstPage.json",
+  });
+  cy.visit(URL);
+});
+
+it("checks number of pages in pagination", () => {
+  cy.get("#pagination").find(".page-link").should("have.length", numberOfPages);
+});
+
+it("checks number of pokemon cards", () => {
+  cy.get("#pokemon-cards").find(".card").should("have.length", cardsPerPage);
+});
+
+/* it("changes page", () => {
+  cy.get("#pagination")
+    .find(".page-link")
+    .then((pages) => {
+      let $pages = [];
+
+      pages.each(function (i, page) {
+        $pages.push(page);
+      });
+      console.log($pages);
+      let defaultCards = [];
+      cy.get(".card").then((cards) => {
+        cards.each(function (i, card) {
+          defaultCards.push(card);
+        });
+        console.log(defaultCards);
+        
+
+        $pages[1].click();
+
+        I expect to get the newCards
+        cy.get(".card");
+      });
+    });
+}); */
+
+/* it("loads cards from fixture", () => {
+  cy.intercept("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1126", {
+    fixture: "firstPage.json",
+  });
+}); */
