b87994969fe5f6bfbc0f403be6193a2ab724896c
Jose <jose_esteban_basualdo@hotmail.com>
2022-07-06 17:13:40 -0300

    adds pagimation module

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ import { getPokemons, getPokemonById } from "./services/services.js";
 
 import {
   createPokemonsCards,
-  createPagination,
   clearPreviousElements,
   setDetailModal,
   clearCards,
   showLoader,
-  handlePagination,
-  handleSelectedPage
 } from "./ui/ui.js";
+
+import {
+  createPagination,
+  handlePagination,
+  handleSelectedPage,
+} from "./pagination/pagination.js";
 
 async function initialize(offset = 0) {
   let pages;
@@ -26,15 +29,17 @@ async function initialize(offset = 0) {
 
   pages = Math.ceil(totalPokemons / pokemonsPerPage);
 
-  createPagination(pages, updatePokemonsCards, handlePagination,handleSelectedPage);
+  createPagination(
+    pages,
+    updatePokemonsCards,
+    handlePagination,
+    handleSelectedPage
+  );
 
   createPokemonsCards(pokemons, handlePokemonDetails);
 }
 
-async function updatePokemonsCards(offset, e) {
-  // console.log(e.target);
-  //stops anchor in pagination of changing URL #
-  // e.preventDefault();
+async function updatePokemonsCards(offset) {
   let pokemonsData = await getPokemons(offset);
   let pokemons = pokemonsData.results;
 
